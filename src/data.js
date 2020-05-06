d30dca4104039d87f0acb556d474120c5c7788aa
NAVIN SUN <navinsun@NAVINs-MacBook-Pro.local>
2020-05-07 01:07:50 +0800

    initial commit

---
+++ src/data.js
@@ -0,0 +1,111 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "iPhone 11 Pro",
+    img: "img/product-1.png",
+    price: 9899,
+    company: "APPLE",
+    info:
+      "The iPhone 11 Pro and iPhone 11 Pro Max use a Super Retina, XDR display, which Apple says is its best display ever in an iPhone. The Super Retina display features support for Dolby Vision, HDR10, and wide color gamut for unparalleled color accuracy.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Samsung S7",
+    img: "img/product-2.png",
+    price: 16,
+    company: "SAMSUNG",
+    info:
+      "Weight:  152g, Dimensions:   142.4 x 69.6 x 7.9 mm, Screen size:  5.1-inch, Resolution:  1440x2560, CPU:  Snapdragon 820/Exynos 8890, RAM:  4GB, Storage:  32GB (with microSD), Battery:  3000mAh, Rear camera:  12MP, Front camera: 5MP.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "HTC 10 - Black",
+    img: "img/product-3.png",
+    price: 8,
+    company: "htc",
+    info:
+      "Released 2016, April 12, Android 6.0.1, up to Android 8.0; Sense UI 32GB/64GB storage, microSDXC,161g, 9mm thickness.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "HTC 10 - White",
+    img: "img/product-4.png",
+    price: 18,
+    company: "htc",
+    info:
+      "Released 2016, April 12, Android 6.0.1, up to Android 8.0; Sense UI 32GB/64GB storage, microSDXC,161g, 9mm thickness.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "HTC Desire 626s",
+    img: "img/product-5.png",
+    price: 24,
+    company: "htc",
+    info:
+      "The HTC Desire 626s comes with a 5-inch 720p display, a 1.1-GHz quad-core processor, 1.5GB of RAM, 8GB of storage, an 8-megapixel autofocus camera and a 2-megapixel camera on the front.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Vintage Iphone",
+    img: "img/product-6.png",
+    price: 17,
+    company: "apple",
+    info:
+      "The display of the iPhone 4 is manufactured by LG under an exclusive contract with Apple. It features an LED backlit TFT LCD capacitive touchscreen with a pixel density of 326 pixels per inch (ppi) on a 3.5 in (8.9 cm) (diagonally measured), 960×640 display.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Iphone 7",
+    img: "img/product-7.png",
+    price: 30,
+    company: "apple",
+    info:
+      "Apple iPhone 7 was launched in Sep.2016. The phone comes with a 4.70-inch touchscreen display with a resolution of 750x1334 pixels at a pixel density of 326 pixels per inch (ppi) and an aspect ratio of 16:9. Apple iPhone 7 is powered by a 2.34GHz quad-core Apple A10 Fusion processor.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Apple Watch",
+    img: "img/product-8.png",
+    price: 3199,
+    company: "apple",
+    info:
+      "Excellent design (and new titanium finish). Fantastic health features, including ECG, Good, comprehensive fitness options, 32GB of on-board storage.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Google Pixel - Black",
+  img: "img/product-1.png",
+  price: 10,
+  company: "google",
+  info:
+    "The Pixel 4 and 4 XL are constructed using an aluminum frame and Gorilla Glass 5. The devices are available in Just Black, Clearly White, and Oh So Orange colors, with the white and orange models having a matte.",
+  inCart: false,
+  count: 0,
+  total: 0
+};
